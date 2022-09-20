3e10a4122488897b732d9f8e69535d489fe630a1
Veronika-Kyr <106926961+Veronika-Kyr@users.noreply.github.com>
2022-09-20 18:39:02 +0300

    Add list of planets and style them as necessary

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+const element = React.createElement("h1", { style: { color: '#999', fontSize: '19px' } }, "Solar system planets:");
 
+const element2 = (<div>
+  {element}
+  <ul className='planets-list'>
+    <li>Mercury</li>
+    <li>Venus</li>
+    <li>Earth</li>
+    <li>Mars</li>
+    <li>Jupiter</li>
+    <li>Saturn</li>
+    <li>Uranus</li>
+    <li>Neptune</li>
+    <li>Pluton</li>
+  </ul>
+</div>);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(element2);
 reportWebVitals();
